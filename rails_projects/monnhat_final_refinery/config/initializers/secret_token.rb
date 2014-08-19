87518a4b44aea242228d5c03de52bbca901d57aa
Tuong Nguyen <nguyentrongtuong1993@yahoo.com>
2014-08-19 14:20:06 +0700

    change menu link status using slug

---
+++ rails_projects/monnhat_final_refinery/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonnhatFinalRefinery::Application.config.secret_token = '6150351f9925f4a59569acac36f996710739757364d34a63c9b405c1be3b0ff683f9892475568386326126c5d5adf064d0c0ba637751bc9db6faab016db5f962'
